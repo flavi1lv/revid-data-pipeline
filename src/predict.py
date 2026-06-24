5fc9b6449ebfd42f7ca5003e0ef9bdbe036d4e79
Flavi1 <p.flavien07@gmail.com>
2026-06-25 00:01:36 +0200

    ajout force nouveau entrainement

--- src/predict.py
+++ src/predict.py
@@ -29,7 +29,7 @@ def predict_spectrogram(image_path, model, class_names):
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
 
-    predictions = model.predict(img_array, verbose=0)*
+    predictions = model.predict(img_array, verbose=0)
     scores = predictions[0]
 
     predicted_class_index = np.argmax(scores)
